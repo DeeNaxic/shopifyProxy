f863202bd28ef72bdfe3ee1779bcf5a656666f5f
DeeNaxic <DeeNaxic@gmail.com>
2025-07-01 15:37:12 +0200

    Update Function1.cs

--- CakeProxy/Function1.cs
+++ CakeProxy/Function1.cs
@@ -9,14 +9,20 @@ public class Function1
     [Function("proxy")]
     public async Task<HttpResponseData> Run ([HttpTrigger(AuthorizationLevel.Anonymous)] HttpRequestData request)
     {
-        var url          = "https://httpbin.org/get";
+        var url          = "https://cphmurano.com/pages/product-feed";
         var client       = new HttpClient();
+
+        client.DefaultRequestHeaders.UserAgent.ParseAdd(
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
+            "(KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36");
+        
+
         var httpResponse = await client.GetAsync(url);
         var content      = await httpResponse.Content.ReadAsStringAsync();
         var response     = request.CreateResponse(HttpStatusCode.OK);
 
-        response.Headers.Add("Content-Type", "application/json");
-        response.WriteString(content);
+        response.Headers.Add("Content-Type", "application/xml");
+        response.WriteString(content.Trim());
 
         return response;
     }
